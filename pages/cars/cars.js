77e9e9d8a6f843678e602cee973b7dd3cf2af015
mura0717 <mura0717@stud.kea.dk>
2023-03-10 21:06:11 +0100

    Fixed endpoint uri

--- pages/cars/cars.js
+++ pages/cars/cars.js
@@ -1,20 +1,25 @@
 import { API_URL } from "../../settings.js"
-const URL = API_URL + "/cars/admin"
+const URL = API_URL + "/cars"
 
 export async function initCars() {
     const cars = await getCars();
-    const carsContainer = document.querySelector(".cars-container");
+    const carsContainer = document.querySelector("#cars-container");
     carsContainer.innerHTML = "";
     cars.forEach(car => {
         carsContainer.innerHTML += `
             <div class="car">
+                p>${car.id}</p>
                 <p>${car.model}</p>
                 <p>${car.brand}</p>
                 <p>${car.model}</p>
                 <p>${car.pricePrDay}</p>
-                <p>${car.bestDiscount}</p>
-                <p>${car.updated}</p>
             </div>
         `
     });
+}
+
+function getCars() {
+    return fetch(URL)
+        .then(res => res.json())
+        .catch(r => console.log(r)
 }
